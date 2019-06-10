1414e8aebdd285b4d8e043399989cfc316455f19
xfdada <39542169+xfdada@users.noreply.github.com>
2019-06-10 11:19:15 +0800

    Update SendSmS.class.php

--- Qcloud/SendSmS.class.php
+++ Qcloud/SendSmS.class.php
@@ -11,10 +11,10 @@ use Qcloud\SmsSingleSender;
 class SendSmS
 {
     // 短信应用SDK AppID
-    private $appid = 1400218923; // 1400开头
+    private $appid = 1400*****; // 1400开头
 
     // 短信应用SDK AppKey
-    private $appkey = "236d613de50948e8c0de937795c14a8a";
+    private $appkey = "***********";
 
     /**单发
      * @param $phoneNumbers     要发送验证码的手机号码
@@ -24,9 +24,9 @@ class SendSmS
 
     public function Send($phoneNumbers, $params)
     {
-//        $phoneNumbers = ["15347974139"];
+//        $phoneNumbers = "13026209544";
 // 短信模板ID，需要在短信应用中申请
-        $templateId = 347726;  // NOTE: 这里的模板ID`7839`只是一个示例，真实的模板ID需要在短信控制台中申请
+        $templateId = *****;  // NOTE: 这里的模板ID`7839`只是一个示例，真实的模板ID需要在短信控制台中申请
 // 签名
         $smsSign = ""; // NOTE: 这里的签名只是示例，请使用真实的已申请的签名，签名参数使用的是`签名内容`，而不是`签名ID`
 // 指定模板ID单发短信
